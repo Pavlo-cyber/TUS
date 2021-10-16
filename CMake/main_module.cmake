6feb77c266bc98f7469d49a47e292b82935303ad
Ivan Manchur <ivan.manchur@globallogic.com>
2021-10-16 20:36:28 +0300

    CMS build

--- CMake/main_module.cmake
+++ CMake/main_module.cmake
@@ -33,12 +33,13 @@ endfunction()
 # ========================
 # Arguments: inputDir    - directory where lies .proto files.
 #            outputDir   - directory where *.pb.cc & *.pb.h will be generated.
+#            additionalIncludePaths - paths to resolve imports in .proto files.
 # Performs: Iterates through every .proto file in <inputDir> and generates
 #           *.pb.h and *.pb.cc files into <outputDir>. [*.pb.h] files will be put into
 #           inc/ directory, [*.pb.cc] files will be put into src directory
 # Returns: GENERATED_SOURCE_FILES - list of all generated source files.
 # ========================
-function(generate_cpp_proto inputDir outputDir)
+function(generate_cpp_proto inputDir outputDir additionalIncludePaths)
     set(GENERATED_SOURCE_FILES "")
     file(GLOB_RECURSE PROTO_FILES ${inputDir} ${inputDir}/*.proto)
     foreach(PROTO_FILE ${PROTO_FILES})
@@ -49,19 +50,25 @@ function(generate_cpp_proto inputDir outputDir)
         list(APPEND GENERATED_SOURCE_FILES ${OUTPUT_SOURCE})
 
     endforeach()
+
+    set(PROTOC_DIRECTORY --proto_path=${inputDir})
+    foreach(includePath ${additionalIncludePaths})
+        set(PROTOC_DIRECTORY "${PROTOC_DIRECTORY};--proto_path=${includePath}")
+    endforeach()
+
     # OUTPUT field needed for file indexing(if some was changed or removed, it will invoke procedure again)
     add_custom_command(OUTPUT
             ${GENERATED_SOURCE_FILES}
             COMMAND
             rm -rf ${outputDir}/inc &&
             rm -rf ${outputDir}/src &&
-            protoc  -I=${inputDir}
+            protoc
             --cpp_out=${outputDir}
-            --proto_path=${inputDir}
+            ${PROTOC_DIRECTORY}
             ${PROTO_FILES} &&
-            protoc  -I=${inputDir}
+            protoc
             --grpc_out=${outputDir}
-            --proto_path=${inputDir}
+            ${PROTOC_DIRECTORY}
             --plugin=protoc-gen-grpc=`which grpc_cpp_plugin`
             ${PROTO_FILES} &&
             python3 ${ROOT_DIRECTORY}/scripts/cpp_separator.py ${outputDir}
@@ -69,4 +76,3 @@ function(generate_cpp_proto inputDir outputDir)
     # return source files to caller
     set(GENERATED_SOURCE_FILES ${GENERATED_SOURCE_FILES} PARENT_SCOPE)
 endfunction()
-
